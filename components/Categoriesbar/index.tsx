39966c71a319329d1bd4afbf0ddba3a657fd781a
Patrick Tunezerwane <tunezepatrick@gmail.com>
2021-12-09 13:24:22 +0200

    ref

---
+++ components/Categoriesbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categoriesbar = () => {
+    return (
+        <div className='categoriesbar'>
+            cats
+        </div>
+    )
+}
+
+export default Categoriesbar
